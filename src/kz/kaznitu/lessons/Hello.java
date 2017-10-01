7085bbfe3451014260a543ac970522c3279e013a
Sabirzhan <sabirjan98@bk.ru>
2017-10-01 14:39:14 +0600

    Екінші коммит

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,8 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHub-pen жұмыс жасап үйрену.");
+
     }
 }
